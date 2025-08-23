02f9c64820255ae2ac5aea1e6b02f772e2c15e62
Vaibhav <you@example.com>
2025-08-24 00:03:29 +0530

    create search component with ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,19 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './pages/search/search.component';
+import { SearchResultComponent } from './pages/search-result/search-result.component';
+import { BookTicketComponent } from './pages/book-ticket/book-ticket.component';
+import { MyBookingsComponent } from './pages/my-bookings/my-bookings.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "search",
+        pathMatch: "full"
+    },
+    { path: "search", component: SearchComponent },
+    { path: "search-result", component: SearchResultComponent },
+    { path: "book-ticket", component: BookTicketComponent },
+    { path: "my-booking", component: MyBookingsComponent },
+    { path: "**", component: PageNotFoundComponent }
 ];
