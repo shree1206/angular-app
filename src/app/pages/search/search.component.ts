4986b88470d7324814bc7d6f41e02ec3840f3403
Vaibhav <you@example.com>
2025-08-24 18:34:54 +0530

    fetch cities from API on search coomponent

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,6 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
+  http = inject(HttpClient);
+  locationList: any[] = [];
+
+  ngOnInit() {
+    this.getLocationList();
+  }
+
+  getLocationList() {
+    this.http.get('https://api.freeprojectapi.com/api/BusBooking/GetBusLocations').subscribe((res: any) => {
+      this.locationList = res;
+    })
+  }
 }
